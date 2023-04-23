0bf9b1971235d7a7ab8861688fafae4be1a48c79
Serhii Nikitin <botodnepr@gmail.com>
2023-04-23 18:42:13 +0200

    [Task-10] log all* requests properties

--- bff-serivce/src/index.ts
+++ bff-serivce/src/index.ts
@@ -1,15 +1,48 @@
 import express, { Express, Request, Response } from "express";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+
 // import type {Request, Response} from 'express'
 
 dotenv.config();
 
-const app: Express = express();
-const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+const app: Express = express();
+app.use(bodyParser.json());
+app.use(cors());
+const port = process.env.PORT || 3000;
+
+app.all("/*/:param", async (req, res) => {
+  // const recipientName = req.params.recipient.toLowerCase();
+  const url = req.originalUrl
+  console.log(req.originalUrl);
+  console.log(req.params);
+  console.log(req.query);
+
+  console.log( req.body);
+  console.log(req.method);
+
+  if(req.originalUrl.startsWith("/product")){
+
+  }
+  else if(req.originalUrl.startsWith("/cart")){
+
+  }
+  res.json({
+    urlOriginal: req.originalUrl,
+    url: req.url,
+    params: req.params,
+    query: req.query,
+    body: req.body,
+    method: req.method
+
+  })
 });
+
+// app.get('/', (req: Request, res: Response) => {
+//   res.send('Express + TypeScript Server');
+// });
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
