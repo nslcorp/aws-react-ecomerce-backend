5cba306c31502eb10d4e688310b264ab2a4e509d
Serhii Nikitin <botodnepr@gmail.com>
2023-04-23 10:01:22 +0200

    [Task-10] init bff-service repository

---
+++ bff-serivce/src/index.ts
@@ -0,0 +1,16 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from 'dotenv';
+// import type {Request, Response} from 'express'
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server');
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
